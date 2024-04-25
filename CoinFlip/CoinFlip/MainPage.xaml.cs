d6e8d2315d168fb95852d98bfddfb52a68d5dc6c
mari-mainarte <mariana.mainarte@etec.sp.gov.br>
2024-04-25 12:39:41 -0300

    feat: Added project

---
+++ CoinFlip/CoinFlip/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace CoinFlip
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            CoinPicker.SelectedIndex = 0;
+        }
+
+        private void CoinBtn_Clicked(object sender, EventArgs e)
+        {
+            int randomCoin = new Random().Next(0, 2);
+            if (randomCoin == 0){
+                CoinImage.Source = "cara_moeda.png";
+                if (CoinPicker.SelectedIndex == 0)
+                {
+                    UserMessage.Text = "Você venceu!";
+                }
+                else
+                {
+                    UserMessage.Text = "Você perdeu!";
+                }
+            }else if (randomCoin == 1)
+            {
+                CoinImage.Source = "coroa_moeda.png";
+                if (CoinPicker.SelectedIndex == 1)
+                {
+                    UserMessage.Text = "Você venceu!";
+                }
+                else
+                {
+                    UserMessage.Text = "Você perdeu!";
+                }
+            }
+        }
+    }
+
+}
